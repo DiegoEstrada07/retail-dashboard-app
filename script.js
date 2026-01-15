3e91872abf5714cbe919f7805b5eeca60840c6fb
Diego Estrada <diego.estrada1705@gmail.com>
2026-01-14 18:34:36 -0600

    chore: update for the charts and the ratings

--- script.js
+++ script.js
@@ -32,14 +32,13 @@ function createChart(data) {
     fetchData();
 
 //cart
- function addToCart(image, name, price) {
-            let cart = JSON.parse(localStorage.getItem('cart')) || [];
-            
-            // Check if the product already exists in the cart
-            let existingProduct = cart.find(item => item.name === name);
-            if (existingProduct) {
-                existingProduct.quantity++;
-            } else {
+function addToCart(image, name, price) {
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+let existingProduct = cart.find(item => item.name === name);
+if (existingProduct) {
+    existingProduct.quantity++;
+        } else {
                 cart.push({
                     image: image,
                     name: name,
@@ -48,7 +47,7 @@ function createChart(data) {
                 });
             }
 
-            // Save the cart to localStorage
+            //localStorage
             localStorage.setItem('cart', JSON.stringify(cart));
 
             alert(name + " has been added to your cart");
@@ -58,11 +57,10 @@ function createChart(data) {
             let cartDiv = document.getElementById('cart');
             let total = 0;
 
-            // If the cart is empty
             if (cart.length === 0) {
                 cartDiv.innerHTML = "<p>Your cart is empty</p>";
             } else {
-                cartDiv.innerHTML = ''; // Clear previous content
+                cartDiv.innerHTML = '';
                 cart.forEach(item => {
                     let itemDiv = document.createElement('div');
                     itemDiv.classList.add('cart-item');
@@ -137,4 +135,133 @@ function createChart(data) {
         }
 
         // Call the function to display the cart when the page loads
-        window.onload = displayCart;
+            window.onload = displayCart;
+
+
+//changin pages
+const params = new URLSearchParams(window.location.search);
+const from = params.get("from");
+
+const imagen = document.getElementById("imagen");
+const texto1 = document.getElementById("texto1");
+const texto2 = document.getElementById("texto2");
+const boton = document.getElementById("boton");
+const price = document.getElementById("price");
+
+if (from === "g-tshirt") {
+    imagen.src = "../asset/products/cloths/green-tshirt.png";
+    texto1.textContent = "Athletic Works Men's Slim Fit T-Shirt";
+    texto2.textContent = "$10.00";
+} else if (from === "g-shorts") {
+    imagen.src = "../asset/products/cloths/green-shorts.png";
+    texto1.textContent = "Athletic Works Men's Mesh Short";
+    texto2.textContent = "$10.00";
+} 
+else if (from === "b-tshirt") {
+    imagen.src = "../asset/products/cloths/blue-tshirt.png";
+    texto1.textContent = "Athletic Works Women's T-Shirt";
+    texto2.textContent = "$14.00";
+} else if (from === "nest") {
+    imagen.src = "../asset/products/electronics/google-nest.png";
+    texto1.textContent = "Google Nest Battery Doorbell";
+    texto2.textContent = "$196.38";
+} else if (from === "samgung") {
+    imagen.src = "../asset/products/electronics/samsung-frame.png";
+    texto1.textContent = "Samsung The Frame 85 QLED 4K TV";
+    texto2.textContent = "$4,798.00";
+} else if (from === "apple") {
+    imagen.src = "../asset/products/electronics/apple-watch.png";
+    texto1.textContent = "Apple Watch Series 11 GPS 42mm Rose Gold Aluminium Case";
+    texto2.textContent = "$549.00";
+} else if (from === "g-pillow") {
+    imagen.src = "../asset/products/home/grey-pillow.png";
+    texto1.textContent = "Bouclair Sage Taha Decorative Pillow 19x19 in (48x48 cm)";
+    texto2.textContent = "$9.99";
+} else if (from === "towel") {
+    imagen.src = "../asset/products/home/bath-towels.png";
+    texto1.textContent = "Canadian Linen Hotel White Bath Towels Set 27 x54  inches 700 GSM Soft Absorbent Quick Dry Ring Spun Cotton Bath Towels for Bathroom Motel Gym Spa Shower Salon& Spa, Pool";
+    texto2.textContent = "$49.97";
+} else if (from === "linen-pillows") {
+    imagen.src = "../asset/products/home/linen-pillows.png";
+    texto1.textContent = "Canadian Linen Microfiber Pillow Cases, 20 x30 Queen, 2 pack, Soft Breathable Brushed Microfiber Pillow Cases Set of 2, Bedding Pillow Covers, White";
+    texto2.textContent = "$18.39";
+} else if (from === "lego") {
+    imagen.src = "../asset/products/lunar-new-year/lego.png";
+    texto1.textContent = "LEGO Spring Festival Galloping Horse Canvas Kids Building Toy - Year of the Horse Toy for Girls & Boys, Ages 10+ - Buildable Chinese New Year Decoration - Gift Idea for Birthdays & Families - 80119";
+    texto2.textContent = "$139.86";
+} else if (from === "jewerly") {
+    imagen.src = "../asset/products/lunar-new-year/jewerly.png";
+    texto1.textContent = "Silvertone July Ruby Red Birthstone Teardrop Necklace";
+    texto2.textContent = "$29.99";
+} else if (from === "horse") {
+    imagen.src = "../asset/products/lunar-new-year/chinise-horse.png";
+    texto1.textContent = "Chinese New Year Horse Design Honeycomb Paper Centerpiece, Color: Multi-Color, Size: 10 , 1 Count, Brand: Way To Celebrate , Decoration";
+    texto2.textContent = "$2.98";
+} else if (from === "ivory") {
+    imagen.src = "../asset/products/personal-care/ivory.png";
+    texto1.textContent = "Ivory Bar Soap Original Scent, 10 x 90 g Bars";
+    texto2.textContent = "$6.73";
+} else if (from === "irish") {
+    imagen.src = "../asset/products/personal-care/irish-spring.png";
+    texto1.textContent = "Irish Spring Original Clean Deodorant Bar Soap for Men, 104.7 g, 6 Pack, 6 Pack";
+    texto2.textContent = "$6.78";
+} else if (from === "nivea") {
+    imagen.src = "../asset/products/personal-care/nivea-men.png";
+    texto1.textContent = "NIVEA Men Cool Kick 24H Fresh Effect Shower Gel for Men | 3-in-1 Body, Face, and Hair Wash | Refreshing Menthol Shower Gel | Dermatologically tested, 500 mL";
+    texto2.textContent = "$4.98";
+}
+else {
+    imagen.src = "";
+    texto1.textContent = "Unknow";
+    texto2.textContent = "undetected";
+}
+
+//Ratings
+
+let selectedRating = 0;
+const stars = document.querySelectorAll("#stars span");
+const statusText = document.getElementById("status");
+
+stars.forEach(star => {
+    star.addEventListener("click", () => {
+        selectedRating = star.getAttribute("data-value");
+
+        stars.forEach(s => s.classList.remove("active"));
+        for (let i = 0; i < selectedRating; i++) {
+            stars[i].classList.add("active");
+        }
+    });
+});
+
+document.getElementById("submitRating").addEventListener("click", () => {
+    const comment = document.getElementById("comment").value;
+
+    if (selectedRating === 0) {
+        statusText.textContent = "Please select a rating.";
+        return;
+    }
+
+    const ratingData = {
+        productId: "product_123",
+        rating: Number(selectedRating),
+        comment: comment,
+        date: new Date().toISOString()
+    };
+
+    fetch("http://localhost:3000/api/ratings", {
+        method: "POST",
+        headers: {
+            "Content-Type": "products/json"
+        },
+        body: JSON.stringify(ratingData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        statusText.textContent = "Thank you for your rating!";
+        console.log(data);
+    })
+    .catch(error => {
+        statusText.textContent = "Error sending the rating.";
+        console.error(error);
+    });
+});
